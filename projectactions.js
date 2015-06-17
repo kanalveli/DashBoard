93dfc5371063b2919a2790d3d85a1fb12595e899
kanal <kanalveli@freshdesk.com>
2015-06-17 19:53:53 +0530

    improved

--- projectactions.js
+++ projectactions.js
@@ -1,8 +1,35 @@
 
 $(document).ready(function(){
-	$("#chatbox").hide();
+	
+	$("#fullscreentoggle").click(function(){
+		  el = document.documentElement
+         , rfs =
+	           el.requestFullScreen
+	        || el.webkitRequestFullScreen
+	        || el.mozRequestFullScreen
+	        || el.msRequestFullscreen;
+		rfs.call(el);
+
+	});
+	$('[data-toggle="tooltip"]').tooltip(); 	
  	$("#chaton").click(function(){
-		$("#chatbox").fadeToggle();
+		$(this).blur();
+		$("#chatbox").animate({right:"0"});
+	});
+	$("#back").click(function(){
+		$(this).blur();
+		$("#chatbox").animate({right:"-200"});
+	});
+	$("#back-chat").click(function(){
+		$(this).blur();
+		$("#chattab").animate({right:"-200"})
+		$("#chattab").children("img,span").remove();
+		$("#chatbox").animate({right:"0"});
+	});
+	$(".chatitem").click(function(){
+		$("#chatbox").animate({right:"-200"});
+		$("#chattab").prepend($(this).children("div").html());
+		$("#chattab").animate({right:"0"});
 
 	});
 	$("#post").keypress(function(ev){
